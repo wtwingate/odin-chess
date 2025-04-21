acf2bbffcdc364c12456c672bf5b20a824bcb48a
William Wingate <wtwingate@gmail.com>
2025-04-21 12:28:43 -0400

    Write methods and tests for King class

--- lib/king.rb
+++ lib/king.rb
@@ -9,6 +9,21 @@ class King < Piece
     @moveset = Deltas::KING
   end
 
+  def in_check?(board)
+    enemy_color = @color == :white ? :black : :white
+    enemy_pieces = board.pieces(enemy_color)
+    enemy_pieces.any? { |piece| piece.moves(board).include?(square) }
+  end
+
+  def moves(board)
+    @moveset.each_with_object([]) do |delta, moves|
+      target = @square + delta
+      next unless board.targetable_square?(target, @color)
+
+      moves << target
+    end
+  end
+
   def ascii
     @color == :white ? "K" : "k"
   end
